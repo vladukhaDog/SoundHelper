526d5fd49d4891a5537c8036824fd0b3fd7e0ebc
Vladislav Permyakov <vladic.p@gmail.com>
2022-02-07 16:48:17 +0300

    Initial Commit

---
+++ SoundTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundTest
+//
+//  Created by Permyakov Vladislav on 07.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SoundTest/SoundTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundTestApp.swift
+//  SoundTest
+//
+//  Created by Permyakov Vladislav on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SoundTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
